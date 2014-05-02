5487885dcb7a2a91e6cfa05974ecbd9b83f29ba3
Alex Jarkey <alex.jarkey@planetinnovation.com.au>
2014-05-02 16:54:15 +1000

    Added test project and framework

---
+++ Example.Common/Example.Common.Test/CounterTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Example.Common.Test
+{
+	[TestFixture ()]
+	public class CounterTest
+	{
+		[Test ()]
+		public void Initial ()
+		{
+			Counter target = new Counter ();
+
+			Assert.AreEqual (0, target.Value(), "The initial Value was not zero");
+		}
+			
+		[Test ()]
+		public void Increment ()
+		{
+			Counter target = new Counter ();
+
+			Assert.AreEqual (0, target.Value(), "The initial Value was not zero");
+
+			target.Increment ();
+
+			Assert.AreEqual (1, target.Value(), "The incremented Value was not 1");
+		}
+	}
+}
+
