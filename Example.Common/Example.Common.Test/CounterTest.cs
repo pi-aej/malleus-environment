2609b96c37b9b03c1b1e1ec9a1315d60988ccfad
Alex Jarkey <alex.jarkey@planetinnovation.com.au>
2014-05-20 14:23:39 +1000

    Fixed unit tests in Example.Common

--- Example.Common/Example.Common.Test/CounterTest.cs
+++ Example.Common/Example.Common.Test/CounterTest.cs
@@ -25,6 +25,22 @@ namespace Example.Common.Test
 
 			Assert.AreEqual (1, target.Value(), "The incremented Value was not 1");
 		}
+
+		[Test ()]
+		public void IncrementTwice ()
+		{
+			Counter target = new Counter ();
+
+			Assert.AreEqual (0, target.Value(), "The initial Value was not zero");
+
+			target.Increment ();
+
+			Assert.AreEqual (1, target.Value(), "The incremented Value was not 1");
+
+			target.Increment ();
+
+			Assert.AreEqual (2, target.Value(), "The incremented Value was not 2");
+		}
 	}
 }
 
