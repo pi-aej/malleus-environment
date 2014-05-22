b3fb6b22aabc746aff0d26e586c60d8b03588139
Alex Jarkey <alex.jarkey@planetinnovation.com.au>
2014-05-22 12:19:31 +1000

    Fluent binding only supports object properties. Events, methods and members all need to be bound using reflection.

--- Example.iOS/Example.iOS/Example_iOSViewController.cs
+++ Example.iOS/Example.iOS/Example_iOSViewController.cs
@@ -38,12 +38,18 @@ namespace Example.iOS
 
 			try
 			{
-				var button = Main_Button;
-
 				var set = this.CreateBindingSet<Example_iOSViewController, Counter>();
 
-				set.Bind(button).For("TouchUpInside").To("IncrementCommand");
-				set.Bind(button).For("Title").To ("ValueStatement");
+				//Binding to events and methods is done with reflection, so strong typing is not possible
+				set.Bind(Main_Button)
+					.For("TouchUpInside")
+					.To("IncrementCommand");
+
+				//Binding to members is requires reflection too, so a NotifyPropertyChanged event can be raised.
+				set.Bind(Main_Button).For("Title").To(vm => vm.ValueStatement);
+
+				//Binding to properties can be done statically, so strong typing is possible
+				set.Bind(Main_Label).For(v => v.Text).To(vm => vm.ValueStatement);
 				set.Apply();
 			}
 			catch(Exception ex) {
